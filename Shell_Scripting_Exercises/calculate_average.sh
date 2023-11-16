f222a28d71a126bc6e53dd7fe5d0ee7a38da038d
EmirhanImranTuzel <emrhntzl@gmail.com>
2023-11-16 15:31:24 +0100

    calculation_average.sh updated

--- Shell_Scripting_Exercises/calculate_average.sh
+++ Shell_Scripting_Exercises/calculate_average.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 read n
-sum=0
 
 if (( n >= 1 && n <= 500 )); then
+    sum=0
     for ((i=0; i<n; i++)); do
         read num
         (( num >= -10000 && num <= 10000 )) && ((sum+=num))
     done
+    average=$(awk -v sum=$sum -v n=$n 'BEGIN {printf "%.3f", sum/n}')
+    echo $average
 fi
-
-average=$(echo "scale=3; $sum / $n" | bc)
-echo $average
